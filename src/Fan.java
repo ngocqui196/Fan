a8301029627051235970ceded426039825578681
Nguyễn Khắc Qui <hivongmotloihua@gmail.com>
2020-09-14 21:15:13 +0700

    Fan-jv

---
+++ src/Fan.java
@@ -0,0 +1,86 @@
+public class Fan {
+    private int SLOW = 1;
+    private int MEDIUM = 2;
+    private int FAST = 3;
+    private int speed;
+    private boolean on;
+    private double radius;
+    private String color;
+
+    public Fan() {
+        this.speed = SLOW;
+        this.on = false;
+        this.radius = 5.0;
+        this.color = "blue";
+    }
+
+    public int getSLOW() {
+        return SLOW;
+    }
+
+    public void setSLOW(int SLOW) {
+        this.SLOW = SLOW;
+    }
+
+    public int getMEDIUM() {
+        return MEDIUM;
+    }
+
+    public void setMEDIUM(int MEDIUM) {
+        this.MEDIUM = MEDIUM;
+    }
+
+    public int getFAST() {
+        return FAST;
+    }
+
+    public void setFAST(int FAST) {
+        this.FAST = FAST;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+      if (this.on) {
+          return "Tốc dộ quạt: " + this.speed +
+                  ", màu quạt: " + this.color +
+                  ", bán kính quạt: " + this.radius +
+                  ", fan is on";
+      }else {
+          return "Màu quạt: " + this.color +
+                  ", bán kính quạt: " + this.radius +
+                  ", fan is off";
+      }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Fan fan = new Fan();
+        System.out.println(fan.toString());
+        fan.setOn(true);
+        System.out.println(fan.toString());
+    }
+}
